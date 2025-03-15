ea89f3c20fb834e0c74790506c69a0028f97b3e3
Gabriel <202502594755@alunos.estacio.br>
2025-03-15 11:53:40 -0300

    Update Super-trunfo.c
    Substituição dos valores area de float para double.

--- Super-trunfo.c
+++ Super-trunfo.c
@@ -3,7 +3,7 @@
 int main() {
     int populacao;
     int numero_de_pontos_turisticos;
-    float area;
+    double area;
     float pib;
     
     printf("Qual a população da cidade?\n");
@@ -11,16 +11,26 @@ int main() {
     printf("Quantos pontos turísticos tem na cidade?\n");  
     scanf("%d", &numero_de_pontos_turisticos);  
     printf("Qual a área da cidade em km²?\n");  
-    scanf("%f", &area);
+    scanf("%d", &area);
     printf("Qual o PIB da cidade?\n");
     scanf("%f", &pib);
+ 
     printf("População %d\n", populacao);
     printf("Pontos túristicos: %d\n", numero_de_pontos_turisticos);
-    printf ("Area: %.2f km²\n", area);
+    printf ("Area: %d km²", area);
     printf("PIB: R$%.2f",pib);
-    printf("Digite sua idade: \n");
-    scanf("%d", &idade);
-    printf("Sua idade é: %d\n", idade);
+
+
+
+ 
+    
+    return 0;
+}
+
+
+
+
+ 
     
     return 0;
 }
