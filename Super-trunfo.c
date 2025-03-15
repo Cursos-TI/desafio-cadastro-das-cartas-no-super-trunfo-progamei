40850ad6795220ff25ccd8d2d04b8cf26ba89773
Gabriel <202502594755@alunos.estacio.br>
2025-03-15 11:36:54 -0300

    Update Super-trunfo.c
    Testando a função de entrada e saída de dados

--- Super-trunfo.c
+++ Super-trunfo.c
@@ -1,1 +1,26 @@
-
+#include <stdio.h>
+ 
+int main() {
+    int populacao;
+    int numero_de_pontos_turisticos;
+    float area;
+    float pib;
+    
+    printf("Qual a população da cidade?\n");
+    scanf("%d", &populacao);
+    printf("Quantos pontos turísticos tem na cidade?\n");  
+    scanf("%d", &numero_de_pontos_turisticos);  
+    printf("Qual a área da cidade em km²?\n");  
+    scanf("%f", &area);
+    printf("Qual o PIB da cidade?\n");
+    scanf("%f", &pib);
+    printf("População %d\n", populacao);
+    printf("Pontos túristicos: %d\n", numero_de_pontos_turisticos);
+    printf ("Area: %.2f km²\n", area);
+    printf("PIB: R$%.2f",pib);
+    printf("Digite sua idade: \n");
+    scanf("%d", &idade);
+    printf("Sua idade é: %d\n", idade);
+    
+    return 0;
+}
